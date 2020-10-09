7f8554f4c42d00e5a5322162bec194b593f88577
Mendim Klimenta <klimenta@stud.hs-heilbronn.de>
2020-10-09 18:53:07 +0200

    Finished CartComponent

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CartService} from '../../services/cart.service';
+import {CartModelServer} from '../../models/cart.model';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
+  cartData: CartModelServer;
+  cartTotal: number;
+  subTotal: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public cartService: CartService) {
   }
 
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+    this.cartService.cartData$.subscribe(data => this.cartData = data);
+    this.cartService.cartTotal$.subscribe(total => this.cartTotal = total);
+  }
+
+  // tslint:disable-next-line:typedef
+  changeQuantity(id: number, increaseQuantity: boolean) {
+    this.cartService.updateCartItems(id, increaseQuantity);
+  }
 }
